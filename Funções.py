af0389472dd8a8c1ff88276fe0988404c50204ec
guiscopellemos <103459685+guiscopellemos@users.noreply.github.com>
2022-04-30 13:57:48 -0300

    Update Funções.py

--- Funções.py
+++ Funções.py
@@ -1,4 +1,26 @@
+#bibliotecas
 import random
+
+#sorteia um pais da lista de paises
 def sorteia_pais(dicionario):
     pais = random.choice(list(dicionario.items()))
     return (pais[0])
+
+#ordena lista de paises
+def adiciona_em_ordem(pais,distancia,lista):
+    pais_formatado = [pais,distancia]
+    i = 0
+    
+    # lista vazia:
+    if len(lista) == 0:
+        return [pais_formatado]
+    
+    #encaixa na poscição correta caso len(lista) > 1:
+    while i < len(lista):
+        pais_da_lista = lista[i]
+        if distancia > pais_da_lista[1]:
+            i += 1
+        else:
+            lista.insert(i,pais_formatado)
+            i = len(lista)
+    return lista
