18aad2a770fe8f853f4874384685b5d6a3377102
guiscopellemos <103459685+guiscopellemos@users.noreply.github.com>
2022-05-13 12:29:45 -0300

    Update jogo_de_paises.py

--- jogo_de_paises.py
+++ jogo_de_paises.py
@@ -11,28 +11,8 @@ from  Funções import *
 DADOS = carrega_dados()
 RAIO = 6371
 
-#inicia o jogo
-quer_jogar = True
-while quer_jogar:
-    #define parametros
-    tentativas = 20
-    distancias = []
-    ar = True
-    pop = True
-    cont = True
-    cor_da_bandeira = []
-    letra_da_capital = []
-    DICAS = {}
-    pais_sorteado = sorteia_pais(DADOS)
-    
-    #informações de pais sorteado
-    dados_pais_sorteado = DADOS[pais_sorteado]
-    capital = dados_pais_sorteado['capital']
-    geo_pais_sorteado = dados_pais_sorteado['geo']
-    coordenadas_pais_sorteado = [geo_pais_sorteado['latitude'],geo_pais_sorteado['longitude']]
-    
-    #tela de inicio
-    print('''
+#tela de inicio
+print('''
  ============================
 |                            |
 |  Bem-vindo ao Acerta País  |
@@ -42,25 +22,65 @@ while quer_jogar:
  ============================ 
 
 comandos:
-
 dica         - Acessa o mercado de dicas 
 desisto      - Desiste da rodada
 inventario   - Exibe sua posição e dicas
 
 Um país foi sorteado, tente descobrir!
 ''')
+
+#inicia o jogo
+quer_jogar = True
+while quer_jogar:
+    #define parametros
+    tentativas = 20
+    distancias = []
+    cor = []
+    ar = True
+    pop = True
+    cont = True
+    foi_dicas = False
+    cor_da_bandeira = []
+    letra_da_capital = []
+    DICAS = {}
+    pais_sorteado = sorteia_pais(DADOS)
+    #print (pais_sorteado)
+    #informações de pais sorteado
+    dados_pais_sorteado = DADOS[pais_sorteado]
+    pega_bandeira = (dados_pais_sorteado['bandeira'])
+    capital = dados_pais_sorteado['capital']
+    geo_pais_sorteado = dados_pais_sorteado['geo']
+    coordenadas_pais_sorteado = [geo_pais_sorteado['latitude'],geo_pais_sorteado['longitude']]
     
+
     #chance de palpite caso tenha tentativas
     while tentativas>0:
-        #parametros locais
 
-        
         # imprime tentatives e pergunta palpite
         print ('Você tem {} tentativa(s)'.format(tentativas))
         pais_palpite = input('Qual é o seu palpite?')
-        
+        if pais_palpite == pais_sorteado:
+            print('Parabéns Você Acertou o País!!!')
+            tentativas = 0
         #palpite é um pais valido
-        if pais_palpite in DADOS:
+        elif pais_palpite == 'inventario':
+            print('Distâncias:')
+            for dist in distancias:
+                print('{:.0f} km -> {}'.format(dist[1],dist[0]))
+            
+            print('Dicas:')
+            for dica,value in DICAS.items():
+                if dica == 'cor_da_bandeira':
+                    print('   - Cores da bandeira: {}'.format(value))
+                if dica == 'letra_da_capital':
+                    print('   - Letras da capital: {}'.format(value))
+                if dica == 'area':
+                    print('   - Área:{} km2'.format(value))
+                if dica == 'populacao':
+                    print('   - População: {} habitantes'.format(value))
+                if dica == 'continente':
+                    print('   - Continente: {}'.format(value))
+        elif pais_palpite in DADOS:
             #calcula distancia do pais sorteado
             dados_pais_palpite = DADOS[pais_palpite]
             geo_pais_palpite = dados_pais_palpite['geo']
@@ -71,7 +91,8 @@ Um país foi sorteado, tente descobrir!
             print('Distâncias:')
             for dist in distancias:
                 print('{:.0f} km -> {}'.format(dist[1],dist[0]))
-            print('Dicas:')
+            if foi_dicas:
+                print('Dicas:')
             for dica,value in DICAS.items():
                 if dica == 'cor_da_bandeira':
                     print('   - Cores da bandeira: {}'.format(value))
@@ -104,47 +125,54 @@ Um país foi sorteado, tente descobrir!
 -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 ''')
             #pede para escolher a dica
-            dica_escolhida = int(input('Escolha sua opção [0|1|2|3|4|5]:'))
-            
-            #dica esta fora das opcoes
-            while dica_escolhida >5  or dica_escolhida <  0:
+            dica_escolhida = input('Escolha sua opção [0|1|2|3|4|5]:')
+            if dica_escolhida == '0' or dica_escolhida == '1' or dica_escolhida == '2' or dica_escolhida == '3' or dica_escolhida == '4' or dica_escolhida == '5':
+                dica_escolhida = int(dica_escolhida)
+            while dica_escolhida != 5 and dica_escolhida != 4 and dica_escolhida != 3 and dica_escolhida != 2 and dica_escolhida != 1 and dica_escolhida != 0:
                 print ('Escolha uma opção valida!')
-                dica_escolhida = int(input('Escolha sua opção [0|1|2|3|4|5]:'))
-      
+                dica_escolhida = input('Escolha sua opção [0|1|2|3|4|5]:')
+                if dica_escolhida == '0' or dica_escolhida == '1' or dica_escolhida == '2' or dica_escolhida == '3' or dica_escolhida == '4' or dica_escolhida == '5':
+                    dica_escolhida = int(dica_escolhida)
             #faz ação da dica:
             if dica_escolhida == 1 and tentativas > 4:
                 custo = 4
-                cor = []
-                pega_bandeira = (dados_pais_sorteado['bandeira'])
+                valor_mais_alto = 0
+                cor_i = ''
                 for cores, valor  in pega_bandeira.items():
-                    if len(cor) == 0:
-                        cor = [cores, valor]
-                    elif valor > cor[1]:
-                        cor = [cores, valor]
-                cor_da_bandeira.append(cor[0])
-                DICAS['cor_da_bandeira'] = cor_da_bandeira
-                tentativas -= custo
+                    if valor > valor_mais_alto:
+                        valor_mais_alto = valor
+                        cor_i = cores
+                if cor_i != 'outras':
+                    pega_bandeira.pop(cor_i)
+                    cor.append(cor_i) 
+                    DICAS['cor_da_bandeira'] = cor
+                    tentativas -= custo
+                foi_dicas = True
             if dica_escolhida == 2 and tentativas > 3:
                 custo = 3
                 letra_sorteada = (sorteia_letra(capital,letra_da_capital))
                 letra_da_capital.append(letra_sorteada)
                 DICAS['letra_da_capital'] = letra_da_capital
                 tentativas-=custo
+                foi_dicas = True
             if dica_escolhida == 3 and ar and tentativas > 6:
                 custo = 6
                 DICAS['area'] = dados_pais_sorteado['area']
                 tentativas -= custo
                 ar = False
+                foi_dicas = True
             if dica_escolhida == 4 and pop and tentativas > 5:
                 custo = 5
                 tentativas -= custo
                 DICAS['populacao'] = dados_pais_sorteado['populacao']
                 pop = False
+                foi_dicas = True
             if dica_escolhida == 5 and cont and tentativas > 7:
                 custo = 7
                 DICAS['continente'] = dados_pais_sorteado['continente']
                 tentativas -= custo
                 cont = False
+                foi_dicas = True
             print('Distâncias:')
             for dist in distancias:
                 print('{:.0f} km -> {}'.format(dist[1],dist[0]))
@@ -165,10 +193,16 @@ Um país foi sorteado, tente descobrir!
         elif pais_palpite == 'desisto':
             quer_desistir = input('Tem certeza que deseja desistir da rodada? [S|N]')
             if quer_desistir == 'S':
-                print("Até Próxima Vez! :) ")
+                #criar lista de respostas para desisto
+                print("Que frouxo/a, o país era: {}".format(pais_sorteado))
+                tentativas = 0
             
             #palpite nao foi reconhecido
         else:
             print('país desconhecido')
-            
-    quer_jogar = False
+    print('Você tem {} tentativas'.format(0))
+    print('Lamentavelmente você perdeu, o país era: {}'.format(pais_sorteado))
+    quer_continuar = input('Jogar Novamente?[S|N]')
+    if quer_continuar != 'S':
+        print('Até a Próxima! :)')
+        quer_jogar = False
